54d0e5402f46ea3f98737025bbd1be972b38f44f
Ersa65 <justh6896@gmail.com>
2024-10-02 14:15:09 +0700

    Create isi.js

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeIn("<center>");
+document.writeIn("<hr width=600 color=Black>");
+document.writeIn("<h1>UNIVERSITAS LAMPUNG</H1>");
+document.writeIn("<h2>PENDIDIKAN TEKNOLOGI INFORMASI</H2>");
+document.writeIn("<h3>JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM</H3>");
+document.writeIn("<hr width=600 color=Black>");
+document.writeIn("</center>");
