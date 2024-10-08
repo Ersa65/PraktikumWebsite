277f5b812b3c44224a1a81d1c584fb6312b33ede
Ersa65 <justh6896@gmail.com>
2024-10-08 23:12:16 +0700

    Create form.js

---
+++ form.js
@@ -0,0 +1,15 @@
+ <section>
+        <h2>Form Pesanan</h2>
+        <form id="pesananForm">
+            <label>
+                <input type="checkbox" name="menu" value="Nasi Bakar"> Nasi Bakar
+            </label><br>
+            <label>
+                <input type="checkbox" name="menu" value="Nasi Goreng"> Nasi Goreng
+            </label><br>
+            <label>
+                <input type="checkbox" name="menu" value="Nasi Uduk"> Nasi Uduk
+            </label><br><br>
+            <button type="button" onclick="kirimPesanan()">Akhiri Pesanan</button>
+        </form>
+    </section>
